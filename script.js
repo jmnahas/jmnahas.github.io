63f4b04b971fa7690dc5a2d9f5fd405bc7319917
jmnahas <90520574+jmnahas@users.noreply.github.com>
2023-12-14 11:12:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var i=0
-colores = [3]
+colores = [2]
+    colores[0]= Math.floor(Math.random()*16777215).toString(16);
     colores[1]= Math.floor(Math.random()*16777215).toString(16);
     colores[2]= Math.floor(Math.random()*16777215).toString(16);
-    colores[3]= Math.floor(Math.random()*16777215).toString(16);
 function oprimir(){
     i=i+1
     if (i==3) {
