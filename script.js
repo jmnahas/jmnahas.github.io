ccadf12b21f3233679b036d90670e7d44132b1fc
jmnahas <90520574+jmnahas@users.noreply.github.com>
2023-12-14 14:38:59 -0300

    Agregue 3 botones y puse mas bonito todo

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ colores = [2]
     colores[0]= Math.floor(Math.random()*16777215).toString(16);
     colores[1]= Math.floor(Math.random()*16777215).toString(16);
     colores[2]= Math.floor(Math.random()*16777215).toString(16);
+function inicio(){
+    document.getElementById("botonsaurio").style.backgroundColor= "#" + colores[i]
+}
+
 function oprimir(){
     i=i+1
     if (i==3) {
