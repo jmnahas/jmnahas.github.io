b80015388b74d590c9be10d4ce671e887701953c
jmnahas <90520574+jmnahas@users.noreply.github.com>
2023-12-14 10:56:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-function oprimir(){
-    colores = [3]
+var i=0
+colores = [3]
     colores[1]= Math.floor(Math.random()*16777215).toString(16);
     colores[2]= Math.floor(Math.random()*16777215).toString(16);
     colores[3]= Math.floor(Math.random()*16777215).toString(16);
-    document.getElementById("botonsaurio").style.color = "#" + colores[1]
+function oprimir(){
+    i=i+1
+    if (i==3) {
+        i=0
+    }
+    
+    document.getElementById("botonsaurio").style.color = "#" + colores[i]
 }
